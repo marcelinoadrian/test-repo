13243183e89921c5024dd9e58d40f2aaf5e6c4cf
marcelinoadrian <marcelino.adrian.g.2030@gmail.com>
2024-03-14 10:23:37 +0800

    Added complete Edit and Delete functions.

--- index.php
+++ index.php
@@ -5,11 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CRUD PHP</title>
     <link rel="stylesheet" href="style.css">
+    <style>
+        
+    </style>
 </head>
 <body>
     <?php
     include 'dbconnection.php';
     $conn = OpenCon();
+
+
+
+    if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_query = mysqli_query($conn, "DELETE FROM `user_data` WHERE id = $delete_id ") or die('query failed');
+
+   if($delete_query){
+      header('location:index.php');
+   } 
+};
+
+ 
+if(isset($_POST['u_submit']))
+{
+    $u_id = $_POST['u_id'];
+    $u_name = $_POST['u_name'];
+    $u_password = $_POST['u_password'];
+    $u_address = $_POST['u_address'];
+    $u_email = $_POST['u_email'];
+
+
+    $update_query = mysqli_query($conn, "UPDATE `user_data` SET full_name = '$u_name', passw = '$u_password', address = '$u_address', email = '$u_email' WHERE id = '$u_id'");
+
+
+    if($update_query)
+    {
+        header("location: ./index.php");
+    }
+    else
+    {
+        echo "Error: " . $sql . "" . mysqli_error($conn);
+    }
+
+     mysqli_close($conn);
+};
+
     ?>
 
     <header>
@@ -39,6 +79,7 @@
                 <th>Password</th>
                 <th>Address</th>
                 <th>Email</th>
+                <th>Action</th>
             </thead>
 
             <?php
@@ -54,6 +95,7 @@
     <td><?php echo $row['passw']; ?></td>
     <td><?php echo $row['address']; ?></td>
     <td><?php echo $row['email']; ?></td>
+    <td><a href="index.php?edit=<?php echo $row['id']; ?>">Edit</a>     <a href="index.php?delete=<?php echo $row['id']; ?>" onclick="return confirm('Remove User from Database?')">Delete</a></td>
 </tr>
 
 
@@ -69,5 +111,49 @@
         </table>
 
     </section>
+
+    <section class="edit-form">
+        <?php
+   
+   if(isset($_GET['edit'])){
+      $edit_id = $_GET['edit'];
+      $edit_query = mysqli_query($conn, "SELECT * FROM `user_data` WHERE id = $edit_id");
+      if(mysqli_num_rows($edit_query) > 0){
+         while($fetch_edit = mysqli_fetch_assoc($edit_query)){
+   ?>
+
+   <form action="" method="POST">
+            
+            <input type="hidden" name="u_id" value="<?php echo $fetch_edit['id']; ?>">
+            <label for="fu-name">Full Name: </label>
+            <input type="text" name="u_name" id="u_name" value="<?php echo $fetch_edit['full_name']; ?> ">
+            <label for="password">Password: </label>
+            <input type="password" name="u_password" id="u_password" value="<?php echo $fetch_edit['passw']; ?> ">
+            <label for="address">Address: </label>
+            <input type="text" name="u_address" id="u_address" value="<?php echo $fetch_edit['address']; ?> ">
+            <label for="email">E-mail: </label>
+            <input type="text" name="u_email" id="u_email" value="<?php echo $fetch_edit['email']; ?> ">
+            <br>
+            <input type="submit" name="u_submit">
+            <br>
+            <input type="reset" value="cancel" id="close-edit" class="btn">
+   </form>
+
+   <?php
+         }
+        }
+         echo "<script>document.querySelector('.edit-form').style.display = 'flex';</script>";
+    }
+   ?>
+    </section>
+
 </body>
+  <script>
+      document.querySelector('#close-edit').onclick = () => 
+      {
+      document.querySelector('.edit-form').style.display = 'none';
+      window.location.href = 'index.php';
+      }    </script>
+
+
 </html>
